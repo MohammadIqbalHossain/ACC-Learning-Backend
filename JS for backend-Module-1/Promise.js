3c846aa69792fc9e69ce0de8dcec811f88216231
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-09-02 16:59:38 +0600

    all the concepts of promises added

--- JS for backend-Module-1/Promise.js
+++ JS for backend-Module-1/Promise.js
@@ -22,13 +22,30 @@ myPromise
 
 
  const userIds = [1, 2, 3, 4, 5, 6];
- const userData = [];
+ let userData = [];
+
+//  for(let i = 0; i < userIds.length; i++){
+//     const newData = userIds[i];
+    
+//     myPromise.then(user => {
+//         userData.push(user);
+//     })
+//  }
 
  for(let i = 0; i < userIds.length; i++){
     const newData = userIds[i];
-    
-    setTimeout(() => {
-        userData.push(newData);
-    }, 500)
+    userData.push(myPromise)
  }
 
+ Promise.all(userData).then(res => {
+    console.log(res)
+ })
+
+//  console.log(userData);
+
+ // Suppose we have to load multiple data from database based on some ids those are comming from frontend. first, we have to loop through the ids. and write a new array to keep all new data are coming for each and every id. We have to do a async database call and push data to the new array. we are using the Promise we wrote before by consuming. I think this would not work. Yes. there arises a problem, we are consuming myPromise consctructor. This is a asyncrounus task when code executes myPromise counsumer would be send to the threat poll and and we quene is empty it will console log "userData". 
+
+ // To solve this problem we can use Promise.all function. First, we have to push promise to userData now it is not a async task. after that line we can call promise.all function it will execute all the data one by one. after that send respone to the frontend.  
+
+
+ 
